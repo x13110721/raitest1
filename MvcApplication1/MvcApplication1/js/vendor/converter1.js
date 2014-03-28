0d3ac5178dbe26df7d48c76e37b458a6e27669e6
sabertooth13 <rkupeed1313@gmail.com>
2014-03-28 03:58:12 -0700

    test1
    dfdfdfdfdf

---
+++ MvcApplication1/MvcApplication1/js/vendor/converter1.js
@@ -0,0 +1,10 @@
+﻿function convert(degree) {
+    if (degree == "C") {
+        F = document.getElementById("c").value * 9 / 5 + 32;
+        document.getElementById("f").value = Math.round(F);
+    }
+    else {
+        C = (document.getElementById("f").value - 32) * 5 / 9;
+        document.getElementById("c").value = Math.round(C);
+    }
+}
